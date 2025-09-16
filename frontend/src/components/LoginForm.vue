0d560ecf3bde8a4a41b9513a7fef1cf03594ca93
Marco Camacho <camaco81@gmail.com>
2025-09-16 13:07:59 -0400

    Correcion de errores en rutas de la api en el login

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -76,7 +76,7 @@ export default {
     async login() {
       this.error = '';
       try {
-        const response = await axios.post('http://localhost:5000/login', {
+        const response = await axios.post('/login', {
           email: this.email,
           password: this.password
         });
